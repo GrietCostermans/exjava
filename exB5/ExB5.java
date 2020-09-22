82b842b1ff1ed01838a7ecccba6f4f84cf2a8696
GrietCostermans <grietje.costermans@bnpparibasfortis.com>
2020-09-22 14:07:26 +0200

    ExB5 Wil niet loop-en

---
+++ exB5/ExB5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class ExB5 { 
+        
+        public static void main(String[] args) {
+
+        Scanner readInput = new Scanner (System.in); 
+        
+        String choosenWelkom = "J";
+
+        do {
+            System.out.println("Welkom!" );
+            System.out.println("Nog eens welkom zeggen? J/N" );
+            choosenWelkom = readInput.nextLine();//.toUpperCase();
+            System.out.println(choosenWelkom);
+        } while (choosenWelkom == "J");
+    }
+
+}
