73501fda47e4f320caea7390032dd4f9cc4e4613
GrietCostermans <grietje.costermans@bnpparibasfortis.com>
2020-09-22 14:37:05 +0200

    ExB3 met for each loop

--- exB3/ExB3.java
+++ exB3/ExB3.java
@@ -13,15 +13,30 @@ public class ExB3 {
         integerList.add(40);
 
         System.out.println("size: " + integerList.size());
-
+        /*
         double sumIntegerList = 0;
+        
         for (int i=0;i<integerList.size();i++) {
             sumIntegerList = (integerList.get(i) + sumIntegerList);
             System.out.println("sumIntegerList : " + sumIntegerList );     
-
-
         }
-        System.out.println("Average of "+sumIntegerList+ " / "+integerList.size()+ " is : " + (sumIntegerList/integerList.size()) );
+        */
+        /* for each loop voorbeeld Sandy
+        	int[] numbers = {3,5,6,8,10};
+		
+		double total = 0;
+		for (int val : numbers){
+			total+=val;
+		}
+        double average = total/numbers.length;
+		System.out.println("The average is: " + average);
+		*/
+        
+        double sumIntegerList = 0;
+        for (int eachValueOfIntegerlist : integerList ) {
+             sumIntegerList+=eachValueOfIntegerlist;
+        }
+        System.out.println("Average of "+sumIntegerList+ " / "+integerList.size()+ " = " + (sumIntegerList/integerList.size()) );
 
         }
 
