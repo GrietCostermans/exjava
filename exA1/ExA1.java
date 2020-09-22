e56880c5247ffd6c368b84682307a5a4d5b4b456
GrietCostermans <grietje.costermans@bnpparibasfortis.com>
2020-09-22 09:58:06 +0200

    ExA1

---
+++ exA1/ExA1.java
@@ -0,0 +1,17 @@
+public class ExA1 { 
+        
+        public static void main(String[] args) {
+            
+        String title = "Griet haar cursus";
+        int days = 5;
+        double price = 500 ;
+        boolean priorKnowledge = false;
+
+        System.out.println("Title: " + title);
+        System.out.println("Days : " + days);
+        System.out.println("Price: " + price);
+        System.out.println("Knowl: " + priorKnowledge);
+
+    }
+
+}
