ecb15832b422ba46e9517c83eb1d6e69a2f8503c
GrietCostermans <grietje.costermans@bnpparibasfortis.com>
2020-09-23 12:35:35 +0200

    ExE1 met CurrentAge erbij maar nog zonder textfile

--- exE1/ExE1.java
+++ exE1/ExE1.java
@@ -7,33 +7,35 @@ import java.io.BufferedWriter;
 public class ExE1 {         
         public static void main(String[] args) {
 
-        Scanner readInput = new Scanner (System.in); 
+            Scanner readInput = new Scanner (System.in); 
         
-        System.out.println("Instructors First Name: " );
-        String instructorFirstName  = readInput.nextLine();
-        System.out.println("Instructors Last Name: " );
-        String instructorLastName  = readInput.nextLine();
-        System.out.println("Instructors Age: " );
-        int instructorAge  = Integer.parseInt(readInput.nextLine());
-        System.out.println("Instructors Starting Age @ ABIS: " );
-        int instructorStartAge  = Integer.parseInt(readInput.nextLine());
-        System.out.println("Instructors Starting Salary @ ABIS: " );
-        double instructorStartSalary  = Double.parseDouble(readInput.nextLine());
+            System.out.println("Instructors First Name: " );
+            String instructorFirstName  = readInput.nextLine();
+            System.out.println("Instructors Last Name: " );
+            String instructorLastName  = readInput.nextLine();
+            System.out.println("Instructors Age: " );
+            int instructorAge  = Integer.parseInt(readInput.nextLine());
+            System.out.println("Instructors Starting Age @ ABIS: " );
+            int instructorStartAge  = Integer.parseInt(readInput.nextLine());
+            System.out.println("Instructors Starting Salary @ ABIS: " );
+            double instructorStartSalary  = Double.parseDouble(readInput.nextLine());
         
-        printSalaryHistory(instructorFirstName,instructorLastName,instructorAge,instructorStartAge,instructorStartSalary);
+            printSalaryHistory(instructorFirstName,instructorLastName,instructorAge,instructorStartAge,instructorStartSalary);
                 
         }
 
         public static void printSalaryHistory(String printFirstName , String printLastName, int printAge, int printStartAge, double printStartSalary) {
         
-        double calculSalary = printStartSalary;
+            double calculSalary = printStartSalary;
+            int i=printStartAge;
 
-        for (int i=printStartAge;i<printStartAge+35;i=i+5)
-         {   System.out.println("Salary of "+printFirstName+" "+printLastName+" at "+i+ " is "+calculSalary ); 
-             calculSalary *= 1.03;
-         }    
+            for (i=printStartAge;(i<printStartAge+35)&&(i<=printAge);i=i+5)
+            {   
+                System.out.println("Salary of "+printFirstName+" "+printLastName+" at "+i+ " is "+calculSalary ); 
+                calculSalary *= 1.03;
+             
+            }    
 
-         System.out.println("Maximum salary reached");
+            if  (i>printStartAge+35) {System.out.println("Maximum salary reached");}
         }
-     
 }
